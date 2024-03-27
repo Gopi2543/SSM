93a9ebceed0c51b376d5438220c0c2b2ffe18888
Gopi D <115570335+Gopi2543@users.noreply.github.com>
2024-03-27 11:14:57 +0530

    Update settings.py

--- SSM/sm/settings.py
+++ SSM/sm/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = SECRET_KEY = 'django-insecure-_c*)&iy^&5z)vt@_t5x0+x#*d=hi71xh1@d*jx3k$u2cy2%#k8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['adminstration.pythonanywhere.com']
 
