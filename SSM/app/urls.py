34c28af07a2eedfa59f8f3afc0f33a7b32f743e6
Gopi D <115570335+Gopi2543@users.noreply.github.com>
2024-03-27 10:10:33 +0530

    Add files via upload

---
+++ SSM/app/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path
+from  . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
+
+urlpatterns = [
+    path('i',views.index,name='index'),
+    path('<int:id>',views.view_student,name='view_student'),
+    path('add/',views.add,name='add'),
+    path('edit/<int:id>',views.edit,name='edit'),
+    path('delete/<int:id>',views.delete,name='delete'),
+
+]
